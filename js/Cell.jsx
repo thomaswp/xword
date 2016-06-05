d7ebd5f29e230f99e90a349fe1eab01bd55566de
Thomas P <thomaswprice@msn.com>
2016-06-05 00:44:35 -0700

    Basic grid

--- js/Cell.jsx
+++ js/Cell.jsx
@@ -1,20 +1,69 @@
 const React = require('react');
+const { StyleSheet, css } = require('aphrodite');
+
+const size = 30;
 
 const Cell = React.createClass({
     
     getInitialState: function() {
         return {
-            letter: this.props.letter
+            letter: this.props.letter,
         }
     },
   
     render: function() {
+        var cellStyles = [styles.cell];
+        if (this.props.selected) cellStyles.push(styles.selected)
         return (
-            <div>
-                {this.state.letter}
+            <div className={css.apply(this, cellStyles)} onClick={this.onClick}>
+                <a className={css(styles.link)} tabIndex="0" ref="text" 
+                        onFocus={this.onFocus}>
+                    {this.state.letter}
+                </a>
             </div>
         );
+    },
+    
+    componentDidMount: function() {
+        this.componentDidUpdate();
+    },
+    
+    componentDidUpdate: function() {
+        if (this.props.selected) {
+            this.onClick();
+        }
+    },
+    
+    onClick: function() {
+        this.refs.text.focus();
+    },
+    
+    onFocus: function() {
+        console.log("f", this.state.letter);
+        this.props.onFocus(this.props.row, this.props.column);
+    },
+    
+    test: function() {
+        
     }
 });
+
+const styles = StyleSheet.create({
+    cell: {
+        height: size,
+        width: size,
+        lineHeight: size + "px",
+        textAlign: "center",
+    },
+    
+    selected: {
+        backgroundColor: "gray", 
+    },
+    
+    link: {
+        outline: 0,
+    },
+});
+
 
 module.exports = Cell;
--- js/grid.jsx
+++ js/grid.jsx
@@ -1,14 +1,57 @@
 const React = require('react');
 const Cell = require('./Cell.jsx');
+const { StyleSheet, css } = require('aphrodite');
 
 const Grid = React.createClass({
+    getInitialState: function() {
+        return {
+            rows: 10,
+            columns: 10,
+            selected: [2, 3],
+        };
+    },
+    
+    // componentWillUpdate: function() {
+    //     this.refs["c" + (r * columns + c)].setState({selected: true});
+    // },
+    
+    childFocused: function(row, column) {
+        this.setState({selected: [row, column]});
+    },
+    
+    shouldComponentUpdate: function(nextProps, nextState) {
+           return JSON.stringify(nextState) != JSON.stringify(this.state);
+    },
+    
     render: function() {
+        const {rows, columns} = this.state;
+        const [sRow, sCol] = this.state.selected;
         return (
-            <div>
-                <Cell letter="A" /><Cell letter="B" /><Cell letter="C" />
-            </div>
+            <table className={css(styles.collapse)} >
+                <tbody>
+                    {[,...Array(rows)].map((x, r) =>
+                        <tr key={r}>
+                        {[,...Array(columns)].map((x, c) =>
+                            <td className={css(styles.collapse)} key={r * columns + c}>
+                                <Cell letter={r+c} key={r * columns + c} 
+                                    ref={"c" + (r * columns + c)} selected={r == sRow && c == sCol}
+                                    onFocus={this.childFocused} row={r} column={c}
+                                />
+                            </td>
+                        )}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         );
     }
+});
+
+const styles = StyleSheet.create({
+    collapse: {
+        borderCollapse: "collapse",
+        border: "thin solid black"
+    },
 });
 
 module.exports = Grid;
